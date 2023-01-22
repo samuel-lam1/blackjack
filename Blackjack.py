ad7e150d593a169b7d70d20e77bb5dee62519c76
Samuel Lam <110639848+samuel-lam1@users.noreply.github.com>
2023-01-22 01:02:08 +0000

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -1,12 +1,13 @@
 '''
 Blackjack
-This game is for 1 vs 1 (Player vs Banker) only.
+This game is for 1 vs 1 (dealer vs player) only.
 '''
 from random import shuffle
 from IPython.display import clear_output
 
 suits = ('spades', 'hearts', 'clubs', 'diamonds')
-values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10, 'A':(1,11)}
+values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,
+            '10':10, 'J':10, 'Q':10, 'K':10, 'A':(1,11)}
 
 class card:
     '''For storing card information'''
@@ -31,34 +32,33 @@ class deck:
     def draw_card(self):
         return self.all_cards.pop()
 
-def display(card,person,point):
+def display(cards,person,point):
     clear_output()
     if person == 'player':
         print("Player's card:\n")
     else:
-        print("Banker's card:\n")
-    for n in range(len(card)):
-        print(f'    {card[n]}')
+        print("Dealer's card:\n")
+    for n in range(len(cards)):
+        print(f'    {cards[n]}')
     print(f'\nTotal points: {point}\n')
 
-def ask_for_draw(card):
+def ask_for_draw(cards):
     choice = 'x'
     while choice not in ('Y','y','N','n'):
         choice = input('Do you need 1 more card? (y/n) ')
         if choice not in ('Y','y','N','n'):
             print('Sorry, invalid input, please try again.\n')
     if choice in ['Y','y']:
-        card.append(mydeck.all_cards.pop())
-        return card, True
-    else:
-        return card, False
+        cards.append(mydeck.all_cards.pop())
+        return cards, True
+    return cards, False
 
-def total_point(card):
+def total_point(cards):
     total = 0
     ace = 0
-    for n in range(len(card)):
-        if card[n].figure != 'A':
-            total += values[card[n].figure]
+    for n in range(len(cards)):
+        if cards[n].figure != 'A':
+            total += values[cards[n].figure]
         else:
             ace += 1
     if ace >= 1: # Choose 11 as the value for Ace if the total value does not exceed 21
@@ -82,42 +82,42 @@ while True:
     mydeck.shuffle()
     player = []
     player_point = 0
-    banker = []
-    banker_point = 0     
-    for n in range(0,2):
+    dealer = []
+    dealer_point = 0
+    for num in range(0,2):
         player.append(mydeck.all_cards.pop())
-        banker.append(mydeck.all_cards.pop())
+        dealer.append(mydeck.all_cards.pop())
     win = 'x'
 
-    draw = True    
+    draw = True
     while len(player) < 5 and total_point(player) <= 21 and draw:
         display(player, 'player', total_point(player))
         player, draw = ask_for_draw(player)
         display(player, 'player', total_point(player))
         if total_point(player) > 21:
-            print("Player's cards exploded!\n")
-            win = 'Banker'
+            print("Player busted!\n")
+            win = 'dealer'
 
     input("\n-------Press Enter to continue.-------\n")
 
     draw = True
-    while len(banker) < 5 and total_point(player) <= 21 and total_point(banker) <= 21 and draw:
-        display(banker, 'banker', total_point(banker))
-        banker, draw = ask_for_draw(banker)
-        display(banker, 'banker', total_point(banker))
-        if total_point(banker) > 21:
-            print("Banker's cards exploded!\n")
+    while len(dealer) < 5 and total_point(player) <= 21 and total_point(dealer) <= 21 and draw:
+        display(dealer, 'dealer', total_point(dealer))
+        dealer, draw = ask_for_draw(dealer)
+        display(dealer, 'dealer', total_point(dealer))
+        if total_point(dealer) > 21:
+            print("Dealer busted!\n")
             win = 'Player'
 
     if win == 'x':
-        if total_point(player) > total_point(banker):
+        if total_point(player) > total_point(dealer):
             win = 'Player'
-        elif total_point(player) < total_point(banker):
-            win = 'Banker'
+        elif total_point(player) < total_point(dealer):
+            win = 'Dealer'
         else:
             win = 'Draw'
 
-    print(f"Player's points: {total_point(player)}, Banker's points: {total_point(banker)}\n")
+    print(f"Player's points: {total_point(player)}, dealer's points: {total_point(dealer)}\n")
     if win == 'Draw':
         print("It's a draw game!")
     else:
